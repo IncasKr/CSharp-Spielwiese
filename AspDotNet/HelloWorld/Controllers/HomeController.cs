ce86d35cf08a036b340228048fa93ac3344db9ab
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-21 13:22:02 +0100

    formating the text in html

--- AspDotNet/HelloWorld/Controllers/HomeController.cs
+++ AspDotNet/HelloWorld/Controllers/HomeController.cs
@@ -9,9 +9,17 @@ namespace HelloWorld.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index(string id)
+        public string Index(string name)
         {
-            return "Hello World MVC, hello " + id;
+            return @"
+                    <html>
+                        <head>
+                            <title>Hello World</title>
+                        </head>
+                        <body>
+                            <p>Hello <span style=""color:red"">" + name + @"</span></p>
+                        </body>
+                    </html>";
         }
     }
 }
