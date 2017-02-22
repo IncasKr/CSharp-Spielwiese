640f960a5edc7dc24c0df73286ef6ed99f319eca
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-22 09:51:56 +0100

    Adding objects Client and Clients

---
+++ AspDotNet/HelloWorld/Models/Clients.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class Clients
+    {
+        public List<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client { Name = "Gladis", Age = 14 },
+                new Client { Name = "Sergio", Age = 9 },
+                new Client { Name = "Marco", Age = 10 }
+            };
+        }
+    }
+}
