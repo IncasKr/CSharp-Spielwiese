e0da067e52d1cd34177b2186725aa2940993c444
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-22 09:29:42 +0100

    Add new view "Index"

--- AspDotNet/HelloWorld/Controllers/HomeController.cs
+++ AspDotNet/HelloWorld/Controllers/HomeController.cs
@@ -9,17 +9,9 @@ namespace HelloWorld.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index(string name)
+        public ActionResult Index()
         {
-            return @"
-                    <html>
-                        <head>
-                            <title>Hello World</title>
-                        </head>
-                        <body>
-                            <p>Hello <span style=""color:red"">" + name + @"</span></p>
-                        </body>
-                    </html>";
+            return View();
         }
     }
 }
