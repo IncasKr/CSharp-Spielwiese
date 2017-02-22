72e648c4dec8da502bfac9e1e9ba1d42231f34f2
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-22 10:10:44 +0100

    Adding 3 views and initialize it

--- AspDotNet/HelloWorld/Controllers/HomeController.cs
+++ AspDotNet/HelloWorld/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,6 +21,26 @@ namespace HelloWorld.Controllers
                 ViewData["Name"] = id;
                 return View();
             }           
+        }
+
+        public ActionResult ListClients()
+        {
+            Clients clients = new Clients();
+            ViewData["Clients"] = clients.GetClients();
+            return View();
+        }
+
+        public ActionResult FindClient(string name)
+        {
+            ViewData["Name"] = name;
+            Clients clients = new Clients();
+            Client client = clients.GetClients().FirstOrDefault(c => c.Name == name);
+            if (client != null)
+            {
+                ViewData["Age"] = client.Age;
+                return View("Found");
+            }
+            return View("NotFound");
         }
     }
 }
