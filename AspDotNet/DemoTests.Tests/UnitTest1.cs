d02bd4e302915c54e3b2359751c3f96fb2abd834
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-23 10:23:51 +0100

    add unit test

---
+++ AspDotNet/DemoTests.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoTests.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
