5f63f1284e9c61d9a61d3b06b0d43750af5c2dca
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-23 11:09:49 +0100

    Adding a weather station

---
+++ AspDotNet/DemoTests/Dal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoTests
+{
+    public class Dal : IDal
+    {
+        public Weather GetWeatherDay()
+        {
+            // hier write a access to the data
+            throw new NotImplementedException();
+        }
+    }
+}
