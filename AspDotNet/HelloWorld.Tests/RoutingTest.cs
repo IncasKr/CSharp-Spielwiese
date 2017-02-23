40ca63263ef76c416bb0e0f150c1d54974431048
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-23 13:41:54 +0100

    Add method test for controller "Home"

---
+++ AspDotNet/HelloWorld.Tests/RoutingTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web;
+using System.Web.Routing;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class RoutingTest
+    {
+        public static RouteData DefineUrl(string url)
+        {
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(c =>
+                c.Request.AppRelativeCurrentExecutionFilePath).Returns(url);
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            return routes.GetRouteData(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void Routes_PageHome2_ReturnHomeControllerAndMethodIndexAndPram2()
+        {
+            RouteData routeData = DefineUrl("~/Home/Index/2");
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("Home", routeData.Values["controller"]);
+            Assert.AreEqual("Index", routeData.Values["action"]);
+            Assert.AreEqual("2", routeData.Values["id"])
+            );
+        }
+    }
+}
