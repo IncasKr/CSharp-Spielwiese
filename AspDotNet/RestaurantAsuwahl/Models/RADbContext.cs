21ef43f68009edebe3a17340d7747c9529db9d36
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-23 15:30:34 +0100

    Adding database context

---
+++ AspDotNet/RestaurantAsuwahl/Models/RADbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantAsuwahl.Models
+{
+    public class RADbContext : DbContext
+    {
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
