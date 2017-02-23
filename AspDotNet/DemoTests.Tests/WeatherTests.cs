5140b486dc908a740b11c383585f7fe9a1caa240
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-23 13:02:47 +0100

    Access to weather data, but occures exception because the method is not implemented.

---
+++ AspDotNet/DemoTests.Tests/WeatherTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoTests.Tests
+{
+    /// <summary>
+    /// Summary description for WeatherTests
+    /// </summary>
+    [TestClass]
+    public class WeatherTests
+    {
+        [TestMethod]
+        public void GetWeatherDay_WithCork_ReturnSun()
+        {
+            IDal dal = new Dal();
+            Weather weatherDay = dal.GetWeatherDay();
+            Assert.AreEqual(25, weatherDay.Temperature);
+            Assert.AreEqual(WeatherType.Sun, weatherDay.Weathers);
+        }
+    }
+}
