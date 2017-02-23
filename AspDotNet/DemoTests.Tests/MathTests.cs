d70bdfc176355d8e3457218b4af33d628e9a067f
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-02-23 10:38:54 +0100

    add methods tests

--- AspDotNet/DemoTests.Tests/MathTests.cs
+++ AspDotNet/DemoTests.Tests/MathTests.cs
@@ -7,8 +7,27 @@ namespace DemoTests.Tests
     public class MathTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Factor_WithNegativeValue_Return1()
         {
+            Assert.AreEqual(1, MathHelper.Factor(-5));
+        }
+
+        [TestMethod]
+        public void Factor_WithValue0_Return1()
+        {
+            Assert.AreEqual(1, MathHelper.Factor(0));
+        }
+
+        [TestMethod]
+        public void Factor_WithValue3_Return6()
+        {
+            Assert.AreEqual(6, MathHelper.Factor(3));
+        }
+
+        [TestMethod]
+        public void Factorielle_AvecValeur15_Retourne1307674368000()
+        {
+            Assert.AreEqual(1307674368000, MathHelper.Factor(15));
         }
     }
 }
