356fd8621679da48f84e5d3af7db5cbd84368a49
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-03-07 16:56:40 +0100

    Creating project eBiB to manage a bibliotech

---
+++ AspDotNet/eBiB.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eBiB.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
