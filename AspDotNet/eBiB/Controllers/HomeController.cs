105c9a997bdbae3a17e011f79472b0c132694111
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-03-23 15:13:36 +0100

    Adding controller "Home"

---
+++ AspDotNet/eBiB/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace eBiB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
