76cc62b873899caa95e4186a12b2813b6e556cd1
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-08-23 11:11:52 +0200

    Testing class Calculator

---
+++ UnitTesting/LogAnTests/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Sum_NoAddCalls_DefaultsToZero()
+        {
+            Calculator calc = new Calculator();
+            int lastSum = calc.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+    }
+}
