643171400f577be05b73ff6ad29b1e9f2d13033c
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-08-23 10:05:56 +0200

    Adding Setup and Teardown methods

--- UnitTesting/LogAnTests/LogAnalyzerTests.cs
+++ UnitTesting/LogAnTests/LogAnalyzerTests.cs
@@ -14,6 +14,12 @@ namespace LogAnTests
             m_analyzer = new LogAnalyzer();
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            m_analyzer = null;
+        }
+
         [Test]
         public void IsValidFileName_validFileLowerCased_ReturnsTrue()
         {
@@ -22,6 +28,13 @@ namespace LogAnTests
             //act
             bool result = analyzer.IsValidLogFileName("whatever.slf");
             //assert
+            Assert.IsTrue(result, "filename should be valid!");
+        }
+
+        [Test]
+        public void IsValidFileName_validFileUpperCased_ReturnsTrue()
+        {
+            bool result = m_analyzer.IsValidLogFileName("whatever.SLF");
             Assert.IsTrue(result, "filename should be valid!");
         }
     }
