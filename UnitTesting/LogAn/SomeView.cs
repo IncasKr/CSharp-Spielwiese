ca11c3ce0abb2dc13d81f006b5708e451c5a5752
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-08-25 12:36:35 +0200

    Cleaning code

--- UnitTesting/LogAn/SomeView.cs
+++ UnitTesting/LogAn/SomeView.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LogAn
 {
@@ -13,7 +9,6 @@ namespace LogAn
         public void TriggerLoad(object o, EventArgs args)
         {
             Load(o, args);
-
         }
     }
 }
