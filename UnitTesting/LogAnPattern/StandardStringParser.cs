c2ad25b5a45bf01d31c75a1801634c6a5c5a938f
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-09-08 12:36:50 +0200

    - implementation of class StandardStringParser - Adding his tests

--- UnitTesting/LogAnPattern/StandardStringParser.cs
+++ UnitTesting/LogAnPattern/StandardStringParser.cs
@@ -8,19 +8,27 @@ namespace LogAnPattern
 {
     public class StandardStringParser : BaseStringParser
     {
-        public StandardStringParser()
+        public StandardStringParser(string value)
         {
-
+            StringToParse = value;
         }
 
         public override string GetTextVersionFromHeader()
         {
-            return null;
+            var tabStrings = StringToParse.Split(';');
+            if (tabStrings.Length < 3)
+            {
+                return null;
+            }
+            return tabStrings[1].Split('=')[1];
         }
 
         public override bool HasCorrectHeader()
         {
-            return false;
+            var tabStrings = StringToParse.Split(';');
+            return tabStrings.Length.Equals(3) 
+                && tabStrings[0].Trim().ToLower().Equals("header") 
+                && tabStrings[1].Split('=')[0].Trim().ToLower().Equals("version");
         }
     }
 }
