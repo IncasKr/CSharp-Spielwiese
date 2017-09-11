a01d4680db4648e52f0477d5f490d3fa5d7e4681
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-09-11 15:31:48 +0200

    Make methods virtual by default: A class that invokes a delegate that can be replaced by a test

---
+++ UnitTesting/LogAnPattern.Tests/UnitTests/MyOverridableClassTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnPattern.Tests.UnitTests
+{
+    [TestFixture]
+    public class MyOverridableClassTests
+    {
+        [Test]
+        public void DoSomething_GivenInvalidInput_ThrowsException()
+        {
+            MyOverridableClass c = new MyOverridableClass();
+            int SOME_NUMBER = 1;
+            //stub the calculation method to return "invalid"
+            c.calculateMethod = delegate (int i) { return -1; };
+            Assert.Throws(typeof(ArithmeticException), ()=> { c.DoSomeAction(SOME_NUMBER); });
+        }
+    }
+}
---
+++ UnitTesting/LogAnPattern/MyOverridableClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnPattern
+{
+    public class MyOverridableClass
+    {
+        public Func<int, int> calculateMethod = delegate (int i)
+        {
+            return i * 2;
+        };
+
+        public void DoSomeAction(int input)
+        {
+            int result = calculateMethod(input);
+            if (result == -1)
+            {
+                throw new ArithmeticException("input was invalid");
+            }
+            //do some other work
+        }
+    }
+}
