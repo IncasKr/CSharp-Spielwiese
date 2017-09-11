4394cc04a391cc492552fda4dfb4b67d768a8430
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-09-11 17:19:12 +0200

    Adding template of testing database without change it

--- UnitTesting/LogAnPattern.Tests/UnitTests/MyOverridableClassTests.cs
+++ UnitTesting/LogAnPattern.Tests/UnitTests/MyOverridableClassTests.cs
@@ -4,10 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace LogAnPattern.Tests.UnitTests
 {
-    [TestFixture]
+    public class RollbackAttribute : Attribute//, ITestAction
+    {
+        private TransactionScope transaction;
+
+        public void BeforeTest(/*TestDetails testDetails*/)
+        {
+            transaction = new TransactionScope();
+        }
+
+        public void AfterTest(/*TestDetails testDetails*/)
+        {
+            transaction.Dispose();
+        }
+
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Test; }
+        }
+    }
+
+    [TestFixture, Rollback]
     public class MyOverridableClassTests
     {
         [Test]
@@ -18,6 +39,12 @@ namespace LogAnPattern.Tests.UnitTests
             //stub the calculation method to return "invalid"
             c.calculateMethod = delegate (int i) { return -1; };
             Assert.Throws(typeof(ArithmeticException), ()=> { c.DoSomeAction(SOME_NUMBER); });
+        }
+
+        [Test, Rollback]
+        public void Write_And_Rollback_IN_DB()
+        {
+
         }
     }
 }
