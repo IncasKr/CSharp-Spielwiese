469c5f400483ffc22be8f12f85dfc803eac0ccd2
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-09-11 10:29:11 +0200

    Anti-pattern: shared-state corruption. The second test (expecting a null return value) will fail because the previous test has already added a number to the Person instance.

---
+++ UnitTesting/LogAnPattern.Tests/UnitTests/SharedStateCorruption.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnPattern.Tests.UnitTests
+{
+    [TestFixture]
+    public class SharedStateCorruption
+    {
+        // Defines sharedPerson state.
+        Person person = new Person();
+
+        [Test]
+        public void CreateAnalyzer_GoodFileName_ReturnsTrue()
+        {
+            person.AddNumber("055-4556684(34)"); // Changes shared state.
+            string found = person.FindPhoneStartingWith("055");
+            Assert.AreEqual("055-4556684(34)", found);
+        }
+
+        [Test]
+        public void FindPhoneStartingWith_NoNumbers_ReturnsNull()
+        {
+            string found = person.FindPhoneStartingWith("0"); // Reads shared state.
+            Assert.IsNull(found);
+        }
+    }
+
+    internal class Person
+    {
+        public string PhoneNumber { get; set; }
+
+        public void AddNumber(string phoneNumber)
+        {
+            PhoneNumber = phoneNumber;
+        }
+
+        public string FindPhoneStartingWith(string startNumbers)
+        {
+            return PhoneNumber.StartsWith(startNumbers) ? PhoneNumber : null;
+        }
+    }
+}
