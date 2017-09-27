8ed33afec28c4643aa005b0bec7baf8d308053b4
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2017-09-27 10:15:25 +0200

    Add project ICDemoTool

---
+++ UnitTesting/ICDemoTool/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using INCAS.Intracall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ICDemoTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btConnection_Click(object sender, RoutedEventArgs e)
+        {
+            btConnection.Content = (btConnection.Content.ToString() == "Connect") ? "Disconnect" : "Connect";
+        }
+    }
+
+    public class BooleanAndConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            foreach (object value in values)
+            {
+                if ((value is bool) && (bool)value == true)
+                {
+                    return false;
+                }
+                else if((value is String) && (String)value == "Connect")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("BooleanAndConverter is a OneWay converter.");
+        }
+    }
+}
