b368acb3eec7e75141998f8977165a7fb808001f
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-12 16:05:43 +0100

    Add WCF service to acces to the data

--- AspDotNet/DTO/PersonEntity.cs
+++ AspDotNet/DTO/PersonEntity.cs
@@ -1,9 +1,17 @@
-﻿namespace DTO
+﻿using System.Runtime.Serialization;
+
+namespace DTO
 {
+    [DataContract]
     public class PersonEntity
     {
+        [DataMember]
         public string ID { get; set; }
+
+        [DataMember]
         public string LastName { get; set; }
+
+        [DataMember]
         public string FirstName { get; set; }
     }
 }
