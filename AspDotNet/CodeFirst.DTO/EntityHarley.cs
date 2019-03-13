72f7573c8e579f3db1aba8cf9543b6cf4b56445f
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-13 12:31:36 +0100

    The table key must be named 'ID' or '<classname>+ID'

--- AspDotNet/CodeFirst.DTO/EntityHarley.cs
+++ AspDotNet/CodeFirst.DTO/EntityHarley.cs
@@ -2,7 +2,7 @@
 {
     public class EntityHarley
     {
-        public int HarleyPK { get; set; }
+        public int ID { get; set; }
         public string Color { get; set; }
         public int Capacity { get; set; }
         public int Power { get; set; }
