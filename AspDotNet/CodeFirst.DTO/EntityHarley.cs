f2fb8fb1a8f92017d5d97a95b66a11f36ecab6de
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-13 11:43:29 +0100

    Adds class that contains information about the futur database table

---
+++ AspDotNet/CodeFirst.DTO/EntityHarley.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.DTO
+{
+    public class EntityHarley
+    {
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+        public int Power { get; set; }
+        public string Model { get; set; }
+    }
+}
