3dd0d72b53defdda7c5c7caff56d927166509b1e
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-13 11:51:13 +0100

    Adds the database context and tells it which class will be used to manage the database table

---
+++ AspDotNet/CodeFirst.DAL/GarageContext.cs
@@ -0,0 +1,10 @@
+﻿using CodeFirst.DTO;
+using System.Data.Entity;
+
+namespace CodeFirst.DAL
+{
+    public class GarageContext : DbContext
+    {
+        public DbSet<EntityHarley> Harleys { get; set; }
+    }
+}
