3dbdcbcaaca474b2105c47b349c6411141cf4998
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-13 12:00:00 +0100

    Creates an Harley provider

---
+++ AspDotNet/CodeFirst.DAL/HarleyProvider.cs
@@ -0,0 +1,23 @@
+﻿using CodeFirst.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeFirst.DAL
+{
+    public class HarleyProvider
+    {
+        public HarleyProvider()
+        {
+
+        }
+
+        public List<EntityHarley> GetAllHarleys()
+        {
+            using (GarageContext context = new GarageContext())
+            {
+                return context.Harleys.ToList();
+            }
+        }
+
+    }
+}
