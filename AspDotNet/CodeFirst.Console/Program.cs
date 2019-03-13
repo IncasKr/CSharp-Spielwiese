e9e72af09491bbda202d7d2acd34ec6a00b97efd
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-13 15:14:35 +0100

    implements methods to update database table

--- AspDotNet/CodeFirst.Console/Program.cs
+++ AspDotNet/CodeFirst.Console/Program.cs
@@ -1,13 +1,18 @@
 ﻿using CodeFirst.DAL;
+using CodeFirst.DTO;
 using System;
 
 namespace CodeFirst.Cons
 {
     class Program
     {
+        private static HarleyProvider harleyProvider;
         static void Main(string[] args)
         {
-            Console.WriteLine("List of harleys");
+            harleyProvider = new HarleyProvider();
+            FillHarleys();
+            ShowVehicles();
+            UpdatingColor("Blue");
             ShowVehicles();
             Console.WriteLine("Fin");
             Console.ReadLine();
@@ -16,11 +21,60 @@ namespace CodeFirst.Cons
 
         private static void ShowVehicles()
         {
-            HarleyProvider harleyProvider = new HarleyProvider();
+            Console.WriteLine("List of harleys");
             foreach (var harley in harleyProvider.GetHarleys())
             {
-                Console.WriteLine($"\t{harley}");
+                Console.WriteLine($"\t| {harley.ID} | {harley.Model} | {harley.Color} | {harley.Capacity} | {harley.Power} |");
             }
+        }
+
+        /// <summary>
+        /// Fills up the harleys into the garage.
+        /// </summary>
+        static void FillHarleys()
+        {
+            Console.WriteLine("===== Filling up the harleys into the garage... =====");
+
+            harleyProvider.Create(new EntityHarley
+            {
+                Color = "Black",
+                Model = "883 Iron",
+                Capacity = 12,
+                Power = 220
+            });
+            harleyProvider.Create(new EntityHarley
+            {
+                Color = "Red",
+                Model = "1200 Nighster",
+                Capacity = 12,
+                Power = 150
+            });
+            harleyProvider.Create(new EntityHarley
+            {
+                Color = "Gray",
+                Model = "1200 Forty height",
+                Capacity = 12,
+                Power = 100
+            });
+            harleyProvider.Create(new EntityHarley
+            {
+                Color = "Black",
+                Model = "Fat boy",
+                Capacity = 25,
+                Power = 200
+            });
+            Console.WriteLine("======================");
+        }
+
+        static void UpdatingColor(string color)
+        {
+            Console.WriteLine("===== Mise à jour de la couleur =====");
+            foreach (EntityHarley harley in harleyProvider.GetHarleys())
+            {
+                harley.Color = color;
+                harleyProvider.Update(harley);
+            }
+            Console.WriteLine("======================");
         }
     }
 }
--- AspDotNet/CodeFirst.DAL/HarleyProvider.cs
+++ AspDotNet/CodeFirst.DAL/HarleyProvider.cs
@@ -6,19 +6,48 @@ namespace CodeFirst.DAL
 {
     public class HarleyProvider
     {
+        private GarageContext context;
         public HarleyProvider()
         {
+            // create the database tables, if it does not exist.
+            context = new GarageContext();
+        }
 
+        private bool Exist(EntityHarley harleyObject)
+        {
+            return context.Harleys.Any(h => 
+                    h.Capacity.Equals(harleyObject.Capacity) &&
+                    h.Color.Equals(harleyObject.Color) &&
+                    h.Model.Equals(harleyObject.Model) &&
+                    h.Power.Equals(harleyObject.Power)
+                );               
+        }
+
+        public bool Create(EntityHarley newHarley)
+        {
+            if (newHarley == null || Exist(newHarley))
+            {
+                return false;
+            }
+            var createdHarley = context.Harleys.Add(newHarley);
+            return context.SaveChanges() > 0;
         }
 
         public List<EntityHarley> GetHarleys()
         {
-            // create the database tables, if it does not exist.
-            using (GarageContext context = new GarageContext())
-            {
-                return context.Harleys.ToList();
-            }
+            return context.Harleys.ToList();
         }
 
+        public bool Update(EntityHarley harleyObject, bool persist = false)
+        {
+            if (harleyObject == null)
+            {
+                return false;
+            }
+            var foundEntity = context.Harleys.SingleOrDefault(h => h.ID.Equals(harleyObject.ID));
+            if (foundEntity == null) return false;
+            context.Entry(foundEntity).CurrentValues.SetValues(harleyObject);
+            return persist ? context.SaveChanges() > 0 : true;                 
+        }
     }
 }
