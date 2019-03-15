214e15704802be96a3d85f3161fe0f3fc8dd502f
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-03-15 14:19:06 +0100

    Creating database using code first without loss old data -Enable migration toupdate the database - command: PM> Enable-Migrations

---
+++ AspDotNet/CodeFirst.DAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CodeFirst.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirst.DAL.GarageContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirst.DAL.GarageContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
