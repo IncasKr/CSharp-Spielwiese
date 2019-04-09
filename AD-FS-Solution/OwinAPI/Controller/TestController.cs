0a76109ada11b7b4c2ed7d12753c6c7b3ef05136
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-04-09 12:56:21 +0200

    Adds classes startup and controller

---
+++ AD-FS-Solution/OwinAPI/Controller/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OwinAPI.Controller
+{
+    class TestController : ApiController
+    {
+        public IEnumerable<string> GetTest()
+        {
+            return new string[] { "One", "Two", "Three" };
+        }
+    }
+}
