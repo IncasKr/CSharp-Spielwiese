1fbd11ce54ae1b737710c0ec92baf3b5b8270798
Gladis Douabalet Maika <gladis.douabaletmaika@stud.hn.de>
2019-04-09 12:44:28 +0200

    solves the localhost ip address

--- AD-FS-Solution/OwinSelfhostSample/Program.cs
+++ AD-FS-Solution/OwinSelfhostSample/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Owin.Hosting;
 using Newtonsoft.Json;
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 
 namespace OwinSelfhostSample
 {
@@ -9,7 +11,7 @@ namespace OwinSelfhostSample
     {
         static void Main(string[] args)
         {
-            string baseAddress = "http://localhost:9000/";
+            string baseAddress = $"http://localhost:9000/";
 
             // Start OWIN host 
             using (WebApp.Start<Startup>(url: baseAddress))
@@ -26,10 +28,62 @@ namespace OwinSelfhostSample
                 
                 var response = client.GetAsync(baseAddress + "api/values").Result;
 
+                Console.WriteLine($"Server starting on {baseAddress}api/values...");
                 Console.WriteLine(response);
                 Console.WriteLine($"\n{response.Content.ReadAsStringAsync().Result}");
                 Console.ReadLine();
             }
+        }
+
+        /// <summary>
+        /// Converts an host name or address to IP address.
+        /// </summary>
+        /// <param name="hostNameOrAddress">The host name or address.</param>
+        /// <returns>The IP address string.</returns>
+        private static string ExtractIPString(string hostNameOrAddress)
+        {
+            string ipString = null;
+            string[] addressComponent = hostNameOrAddress.Split('/');
+            foreach (var partOfAddress in addressComponent)
+            {
+
+                try
+                {
+                    if (string.IsNullOrEmpty(hostNameOrAddress))
+                    {
+                        return null;
+                    }
+                    else if (IPAddress.TryParse(hostNameOrAddress, out IPAddress ipObj))
+                    {
+                        ipString = ipObj.MapToIPv4().ToString();
+                        break;
+                    }
+                    else
+                    {
+                        IPAddress[] ipList;
+                        if (hostNameOrAddress == "localhost")
+                        {
+                            ipList = Dns.GetHostAddresses(Environment.MachineName);
+                            foreach (IPAddress ip in ipList)
+                            {
+                                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                                {
+                                    ipString = ip.ToString();
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ipList = Dns.GetHostAddresses(hostNameOrAddress);
+                            ipString = ipList[ipList.Length - 1].MapToIPv4().ToString();
+                        }
+                        break;
+                    }
+                }
+                catch { }
+            }
+            return ipString;
         }
     }
 }
