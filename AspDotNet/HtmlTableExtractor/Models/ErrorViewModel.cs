6901e3ae225a13fe969d83b76a6366a6ca70e41b
ndouabs <gladis.douabalet@web.de>
2021-05-07 11:58:53 +0200

    Init project HtmlTableExtractor

---
+++ AspDotNet/HtmlTableExtractor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HtmlTableExtractor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
